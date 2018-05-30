c22bc4ab13b86d3dab4e9ecdd652a685b80a7a4c
Mark Johnson <markfjohnson@gmail.com>
2018-05-30 12:13:52 -0400

    Initial commit

---
+++ Installation/install-demo-env.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+dcos package install --yes dcos-enterprise-cli
+dcos package install --yes spark
+dcos package install --yes spark --options=spark-a.json
+#dcos package install --yes spark --options=spark-b
+
+# define the service accounts
+
+
+# define the users, groups and their permissions
+echo "Setup security rules"
+dcos security org groups create dept-a
+dcos security org groups create dept-b
+dcos security org users create -p password meatloaf
+dcos security org users create -p password jsmith
+dcos security org users create -p password jdoe
+dcos security org groups add_user dept-a jsmith
+dcos security org groups add_user dept-b jdoe
+dcos security org groups grant dept-a dcos:adminrouter:service:marathon full
+dcos security org groups grant dept-a dcos:service:marathon:marathon:services:/dept-a full
+dcos security org groups grant dept-a dcos:adminrouter:ops:slave full
+dcos security org groups grant dept-a dcos:mesos:master:framework:role:slave_public read
+dcos security org groups grant dept-a dcos:mesos:master:executor:app_id:/dept-a full
+dcos security org groups grant dept-a dcos:mesos:master:task:app_id:/dept-a full
+dcos security org groups grant dept-a dcos:mesos:agent:framework:role:slave_public read
+dcos security org groups grant dept-a dcos:mesos:agent:executor:app_id:/dept-a read
+dcos security org groups grant dept-a dcos:mesos:agent:task:app_id:/dept-a read
+dcos security org groups grant dept-a dcos:mesos:agent:sandbox:app_id:/dept-a read
+
+dcos security org groups grant dept-b dcos:adminrouter:service:marathon full
+dcos security org groups grant dept-b dcos:service:marathon:marathon:services:/dept-b full
+dcos security org groups grant dept-b dcos:adminrouter:package full
